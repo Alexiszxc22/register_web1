67e75af243d23e6d333049f67671a330e883b5d4
Alexiszxc22 <alexiszxc.wup@gmail.com>
2024-03-05 14:59:39 +0800

    added password and function

--- register.js
+++ register.js
@@ -10,21 +10,36 @@
             //   ]
 // undefined = null            
 
-console.log("Hello World", 123, true, ["apple"]);
-console.error("Hello");
-console.warn("Hello");
+// console.log("Hello World", 123, true, ["apple"]);
+// console.error("Hello");
+// console.warn("Hello");
 // console.table();
 
 let name = true;
 console.log(typeof(name));
 
 let txtUsername = document.querySelector("#txtUsername");
+let txtPassword = document.querySelector("#txtPassword");
+let txtConfirmPassword = document.querySelector("#txtConfirmPassword");
 let btnRegister = document.querySelector("#btnRegister");
 
 btnRegister.onclick = function (){
-    register(txtUsername.value)
+    register(txtUsername.value, txtPassword.value, txtConfirmPassword.value)
 }
 
-function register (username) {
-    console.log(username)
+function register (username, Password, ConfirmPassword) {
+    console.log(username, Password, ConfirmPassword)
+
+    // if.. else if.. if
+    // Conditional Ops(Greater Than >, Less Than <, Equal ==, Not !, or ||
+    // Greater Than or Equal >= , Less Than or Equal <=.
+    // Not Equal != 
+    
+    if(Password == ConfirmPassword && Password != "" && ConfirmPassword !=""){
+      return  console.log("Password Match!")
+    } else if (Password == "" || ConfirmPassword == "" ){
+      return  console.log("Password must not be empty.")
+    }   else {
+     return console.log("Password does not match.")
+    }
 }
